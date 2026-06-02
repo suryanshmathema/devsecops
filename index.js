1a692ebca901a883e2fb77bf8ab572dca615b41b
suryanshmathema <suryansh.mathema@gmail.com>
2026-06-02 12:04:44 +0545

    Modf: index.js and vercel.json

--- index.js
+++ index.js
@@ -2,19 +2,16 @@ const express = require('express');
 const lodash = require('lodash');
 const minimatch = require('minimatch');
 
-
 const app = express();
 
-
-// Route for "/"
 app.get('/', (req, res) => {
-  const obj = lodash.cloneDeep({ message: 'Hello, DevSecOps by Suryansh Mathema!' });
+  const obj = lodash.cloneDeep({
+    message: 'Hello, DevSecOps by Suryansh Mathema!'
+  });
+
   const pattern = minimatch('.js', '.js');
-  res.send(${obj.message} (Pattern match: ${pattern}));
+
+  res.send(`${obj.message} (Pattern match: ${pattern})`);
 });
 
-
-// Export a handler function for Vercel
-module.exports = (req, res) => {
-  app(req, res);
-};
+module.exports = app;
